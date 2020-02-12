cdf7a6020006fad441b6f31d99daf6d0797ceb74
zhouyufei1 <zhou.2153@osu.edu>
2020-02-12 12:42:58 -0500

    aa

--- z/src/App.js
+++ z/src/App.js
@@ -1,8 +1,28 @@
 import React, { Component } from 'react';
 import "./App.css";
+import {Navbar} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Routes.js";
 
 export default class App extends Component {
   render() {
-    return <div>Fisher Bookstore</div>
+    return (
+      <div className= "App container">
+        <Navbar bg= "light" expand= "lg">
+          <Navbar.Brand>
+          <Link to="/">Fisher Bookstore</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="justify-content-end">
+           <Nav>
+            <NavItem>
+             <Nav.Link href="/books">Books</Nav.Link>
+            </NavItem>
+        </Nav>
+       </Navbar.Collapse>
+        </Navbar>
+        <Routes />
+      </div>
+    );
   }
 }

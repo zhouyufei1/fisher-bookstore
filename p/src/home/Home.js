f1373c3220886a9cdc8c7801c1def2b8f368c2e1
zhouyufei1 <zhou.2153@osu.edu>
2020-02-16 22:35:12 -0500

    int

---
+++ p/src/home/Home.js
@@ -0,0 +1,15 @@
+import React from "raect";
+import "./Home.css";
+
+export default function Home() {
+    return(
+        <div className="Home">
+            <div className="lander">
+                <h1>Home</h1>
+                <p> Welcome to Fisher Bookstore</p>
+            </div>
+        </div>
+
+    );
+    
+}

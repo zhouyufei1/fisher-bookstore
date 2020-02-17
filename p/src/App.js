ae44ba1931680dac6993a20913ba24f68fe436d4
zhouyufei1 <zhou.2153@osu.edu>
2020-02-16 21:39:52 -0500

    init

--- p/src/App.js
+++ p/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "./Routes.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default class App extends Component {
+  render() {
+    return( 
+    <div className= "App container">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>
+          <Link to="/">Fisher Bookstore</Link>
+        </Navbar.Brand>
+      </Navbar>
+      <Routes />
     </div>
-  );
+    );
+  }
 }
-
-export default App;

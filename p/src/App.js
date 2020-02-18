d15a008676fdb30ea62f9f1c757baaf30e1feca9
zhouyufei1 <59670835+zhouyufei1@users.noreply.github.com>
2020-02-18 14:22:42 -0500

    Navigation feature (#14)
    * int
    
    * int

--- p/src/App.js
+++ p/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Routes from "./Routes"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand>
+            <Link to="/">Fisher Bookstore</Link>
+          </Navbar.Brand>
+        </Navbar>
+        <Routes />
+      </div>
+    );
+  }
 }
-
-export default App;

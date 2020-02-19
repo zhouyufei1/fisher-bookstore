aa4bf4041227e8a317ce7d73bb80be076f278385
zhouyufei1 <zhou.2153@osu.edu>
2020-02-19 15:22:29 -0500

    a

---
+++ p/src/Routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Home/Home";
+import Books from "./Books/Books";
+
+export default function Routes() {
+    return(
+        <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/books" component={Books} />
+        </Switch>
+    );
+}

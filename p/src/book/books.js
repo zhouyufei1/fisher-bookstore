e81a8c4880b97f00126ad18cca09ccc0b54442b5
zhouyufei1 <zhou.2153@osu.edu>
2020-02-19 15:51:38 -0500

    a

---
+++ p/src/book/books.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Authors.css";
+
+export default function Home() {
+    return(
+        <div className="Home">
+            <div className ="lander">
+                <h1>Authors</h1>
+                <p>Books</p>
+            </div>
+        </div>
+    );
+    }
